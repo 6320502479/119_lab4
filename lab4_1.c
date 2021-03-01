f74898a43724fb914450df3113d452ae750fb9fc
6320502479 <pattrapol.J@ku.th>
2021-03-01 18:01:59 +0700

    f

--- lab4_1.c
+++ lab4_1.c
@@ -3,16 +3,30 @@ int main()
 {
     int n,i,j;
     scanf("%d",&n);
-    int r[n][3];
+    int r[n][3],p[n],p3[n-2];
+    p[0]=0;
     for(i=0;i<n;i++)
     {
         for(j=0;j<3;j++)
+        {
             scanf("%d",&r[i][j]);
+        }
     }
     for(i=0;i<n;i++)
     {
-        for(j=0;j<3;j++)
-            printf("%d ",r[i][j]);
-            printf("\n");
+        p[i]=r[i][0]*4+r[i][1]*2+r[i][2]*1;
     }
+    int max=0,min;
+    for(i=0;i<n-2;i++)
+    {
+        p3[i]=p[i]+p[i+1]+p[i+2];
+        printf("%d ",p3[i]);
+    }
+    for(i=0;i<n-2;i++)
+    {
+        if(max<p3[i])
+            max=i+1;
+    }
+    printf("%d %d",max,min);
+
 }
---
+++ lab4_3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char in[200],out[50];
+    int i,j,n;
+    scanf("%[^\n]s",in);
+    puts(in);
+    n=strlen(in);
+    out[0]=in[1];
+    puts(out);
+    for(i=0;i<n;i++)
+    {
+
+    }
+}
