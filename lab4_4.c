3ee7478c37d14b1f447127252e8e2e9cfee6a3ac
6320502479 <pattrapol.J@ku.th>
2021-03-01 19:17:59 +0700

    gg

--- lab4_4.c
+++ lab4_4.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
 #include <string.h>
+#include<ctype.h>
 int main()
 {
     int i,j,k,n;
-    char in[1000],out[20],ignor[10][5] = {"i", "of", "at", "with", "for", "the", "on", "a", "an", "in"};
+    char *p,in[1000],out[20],ignor[10][5] = {"i", "of", "at", "with", "for", "the", "on", "a", "an", "in"};
     gets(in);
     n=strlen(in);
-    for(i=0;i<n;i++)
+    p=strtok(in," ");
+     while (p != '\0')
     {
-        if(in[i+1]==" ");
+        for (j=0 ; j < 10; j++)
         {
-            out[i]=in[i+2];
+            if (strcmp(p, ignor[j]))
+                k++;
         }
+        if (k == 10 || i == 0)
+        {
+            printf("%c", toupper(p[0]));
+        }
+        p = strtok('\0'," ");
+        i++, k = 0;
     }
-    puts(out);
-
 }
+
+
